6d2a1171d7a1612349b2154a76d34a9cdcd9db35
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-05-05 23:51:14 +0700

    delete plugin in wp

--- wordpress/wp-content/plugins/autobiz-wordpress-plugin/templates/footer-admin.php
+++
@@ -1,5 +0,0 @@
-<script type="application/javascript">
-    jQuery('a[href="admin.php?page=tiledesk-chat"]')
-        .attr('href', '<?php echo esc_url( $redirectUrl ); ?>')
-        .attr('target', '_blank');
-</script>
--- wordpress/wp-content/plugins/autobiz-wordpress-plugin/templates/footer-embed.php
+++
@@ -1,4 +0,0 @@
-<script type="application/javascript">
-    window.tiledeskSettings = <?php echo wp_json_encode( $widget_config ); ?>;
-    (function(e,t,n){var c=window;var e=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(e){i.q.push(e)};c.Tiledesk=i;var r,s=e.getElementsByTagName(t)[0];if(e.getElementById(n))return;r=e.createElement(t);r.id=n;r.async=true;r.src="<?php echo esc_url( $tiledesk_jssdk_url ); ?>";s.parentNode.insertBefore(r,s)})(document,"script","tiledesk-jssdk");
-</script>
